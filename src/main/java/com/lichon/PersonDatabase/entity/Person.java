b040f34b1feec11d3a13994b9fd8cee2d53b3a9f
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-17 10:14:08 +0200

    creates person entity

---
+++ src/main/java/com/lichon/PersonDatabase/entity/Person.java
@@ -0,0 +1,42 @@
+package com.lichon.PersonDatabase.entity;
+
+import java.util.Date;
+
+public class Person {
+
+	private int id;
+	private String name;
+	private Date birthDate;
+
+	public Person(int id, String name, Date birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+}
