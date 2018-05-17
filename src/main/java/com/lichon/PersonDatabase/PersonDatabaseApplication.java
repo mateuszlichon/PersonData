104945e6f2f2c4d287c6681c083d3ca502032b1a
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-17 12:40:37 +0200

    develops log out at app start

--- src/main/java/com/lichon/PersonDatabase/PersonDatabaseApplication.java
+++ src/main/java/com/lichon/PersonDatabase/PersonDatabaseApplication.java
@@ -1,5 +1,7 @@
 package com.lichon.PersonDatabase;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +9,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lichon.PersonDatabase.entity.Person;
 import com.lichon.PersonDatabase.jdbc.PersonJdbcDao;
 
 @SpringBootApplication
 public class PersonDatabaseApplication implements CommandLineRunner {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	PersonJdbcDao dao;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(PersonDatabaseApplication.class, args);
 	}
@@ -25,6 +28,8 @@ public class PersonDatabaseApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		logger.info("All users => {}", dao.findAll());
 		logger.info("User id 10001 => {}", dao.findById(10001));
-		logger.info("Deletin 10002 => {}", dao.deleteById(10002));
+		logger.info("Deleting 10002 => {}", dao.deleteById(10002));
+		logger.info("Insertin 10004 => {}", dao.insertPerson(new Person(10004, "Ktos nowy", "Miejsce", new Date())));
+		logger.info("Updating 10003 => {}", dao.update(new Person(10003, "Asterino", "Nowy kosmos", new Date())));
 	}
 }
