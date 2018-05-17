b1df8688a00f8fa0347f080e80a6d3635d50904e
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-17 10:17:05 +0200

    uses beanpropertymapper to map class from db

--- src/main/java/com/lichon/PersonDatabase/jdbc/PersonJdbcDao.java
+++ src/main/java/com/lichon/PersonDatabase/jdbc/PersonJdbcDao.java
@@ -3,8 +3,11 @@ package com.lichon.PersonDatabase.jdbc;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.lichon.PersonDatabase.entity.Person;
 
 @Repository
 public class PersonJdbcDao {
@@ -13,6 +16,6 @@ public class PersonJdbcDao {
 	JdbcTemplate jdbcTemplate;
 
 	public List<Person> findAll() {
-		jdbcTemplate.query("select * from person", rse)
+		return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
 	}
 }
