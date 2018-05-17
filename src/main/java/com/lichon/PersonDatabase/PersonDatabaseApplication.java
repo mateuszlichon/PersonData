2f4b840ac6133a8953955292b4ae9dd7b69645ef
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-17 10:42:42 +0200

    implements command line runner at main app

--- src/main/java/com/lichon/PersonDatabase/PersonDatabaseApplication.java
+++ src/main/java/com/lichon/PersonDatabase/PersonDatabaseApplication.java
@@ -1,12 +1,28 @@
 package com.lichon.PersonDatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PersonDatabaseApplication {
+import com.lichon.PersonDatabase.jdbc.PersonJdbcDao;
 
+@SpringBootApplication
+public class PersonDatabaseApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	PersonJdbcDao dao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PersonDatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users => {}", dao.findAll());
 	}
 }
